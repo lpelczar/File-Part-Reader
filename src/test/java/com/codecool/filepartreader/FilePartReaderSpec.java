9618f8238a11763132b24acd9c5305c66890f237
lpelczar <lukepelczar@gmail.com>
2018-02-23 17:23:27 +0100

    Add test checking if file path field is properly set

--- src/test/java/com/codecool/filepartreader/FilePartReaderSpec.java
+++ src/test/java/com/codecool/filepartreader/FilePartReaderSpec.java
@@ -1,4 +1,21 @@
 package com.codecool.filepartreader;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class FilePartReaderSpec {
+
+    private FilePartReader filePartReader;
+
+    @Before
+    public void before() {
+        filePartReader = new FilePartReader();
+    }
+
+    @Test
+    public void whenFilePartReaderInstantiatedThenFilePathIsSet() {
+        assertEquals("test_data.txt", filePartReader.getFilePath());
+    }
 }
