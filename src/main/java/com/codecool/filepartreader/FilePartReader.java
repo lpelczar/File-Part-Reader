a0e0a4d1488c4877701c0694821832f6d20c741d
lpelczar <lukepelczar@gmail.com>
2018-02-23 17:26:16 +0100

    Implement passing test checking if file path field is set properly

--- src/main/java/com/codecool/filepartreader/FilePartReader.java
+++ src/main/java/com/codecool/filepartreader/FilePartReader.java
@@ -1,4 +1,14 @@
 package com.codecool.filepartreader;
 
 public class FilePartReader {
+
+    private String filePath;
+
+    public FilePartReader() {
+        this.filePath = "test_data.txt";
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
 }
