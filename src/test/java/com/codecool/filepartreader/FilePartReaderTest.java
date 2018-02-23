484c3d44e86c01f60a7468537470ddf628ae7e22
lpelczar <lukepelczar@gmail.com>
2018-02-23 17:39:23 +0100

    Rename file part reader test class

--- src/test/java/com/codecool/filepartreader/FilePartReaderTest.java
+++ src/test/java/com/codecool/filepartreader/FilePartReaderTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class FilePartReaderSpec {
+public class FilePartReaderTest {
 
     private FilePartReader filePartReader;
 
