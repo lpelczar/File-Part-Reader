d797edc2a2caaf2625a0d7eef6f3aba341afeef8
lpelczar <lukepelczar@gmail.com>
2018-02-23 17:42:40 +0100

    Implement passing test checking 'from line' initialization

--- src/main/java/com/codecool/filepartreader/FilePartReader.java
+++ src/main/java/com/codecool/filepartreader/FilePartReader.java
@@ -3,12 +3,18 @@ package com.codecool.filepartreader;
 public class FilePartReader {
 
     private String filePath;
+    private int fromLine;
 
     public FilePartReader() {
         this.filePath = "test_data.txt";
+        this.fromLine = 2;
     }
 
     public String getFilePath() {
         return filePath;
+    }
+
+    public int getFromLine() {
+        return fromLine;
     }
 }
