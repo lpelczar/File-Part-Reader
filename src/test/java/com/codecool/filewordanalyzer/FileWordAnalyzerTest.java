af8051f0de9b8d584fb61691bfcc7ea3c55ab19c
lpelczar <lukepelczar@gmail.com>
2018-02-25 16:42:51 +0100

    Add test checking when instantiated then filepartreader is set

--- src/test/java/com/codecool/filewordanalyzer/FileWordAnalyzerTest.java
+++ src/test/java/com/codecool/filewordanalyzer/FileWordAnalyzerTest.java
@@ -1,4 +1,23 @@
 package com.codecool.filewordanalyzer;
 
+import com.codecool.filepartreader.FilePartReader;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.junit.Assert.assertThat;
+
 public class FileWordAnalyzerTest {
+
+    FileWordAnalyzer fileWordAnalyzer;
+
+    @Before
+    public void before() {
+        fileWordAnalyzer = new FileWordAnalyzer(new FilePartReader());
+    }
+
+    @Test
+    public void whenInstantiatedThenFilePartReaderIsSet() {
+        assertThat(fileWordAnalyzer.getFilePartReader(), instanceOf(FilePartReader.class));
+    }
 }
