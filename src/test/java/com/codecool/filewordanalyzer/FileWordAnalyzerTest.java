50ac578ade67d553eede4f67e7b2db5556c6d4c5
lpelczar <lukepelczar@gmail.com>
2018-02-25 17:51:24 +0100

    Add test checking if words by abc method returns ordered words

--- src/test/java/com/codecool/filewordanalyzer/FileWordAnalyzerTest.java
+++ src/test/java/com/codecool/filewordanalyzer/FileWordAnalyzerTest.java
@@ -4,6 +4,10 @@ import com.codecool.filepartreader.FilePartReader;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class FileWordAnalyzerTest {
@@ -18,5 +22,15 @@ public class FileWordAnalyzerTest {
     @Test
     public void whenInstantiatedThenFilePartReaderIsSet() {
         assertNotNull(fileWordAnalyzer.getFilePartReader());
+    }
+
+    @Test
+    public void whenWordsByABCThenReturnWordsOrderedAlphabeticallyAsArrayList() {
+        fileWordAnalyzer.getFilePartReader().setFilePath("test_data.txt");
+        fileWordAnalyzer.getFilePartReader().setFromLine(1);
+        fileWordAnalyzer.getFilePartReader().setToLine(3);
+        ArrayList<String> expected = new ArrayList<>(Arrays.asList("1a1", "2a", "2b", "3a", "3b", "3c"));
+        ArrayList<String> result = fileWordAnalyzer.wordsByABC();
+        assertArrayEquals(expected.toArray(), result.toArray());
     }
 }
